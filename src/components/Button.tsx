d0828e9a05a3d4a1e582e95881fe91235d875799
Natanael Lima <taelima1997@gmail.com>
2021-07-03 10:12:02 -0300

    fix eslint errors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,41 +1,34 @@
 import React from 'react';
 import {
-    TouchableOpacity,
-    TouchableOpacityProps,
-    Text,
-    StyleSheet
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  StyleSheet,
 } from 'react-native';
 
 interface ButtonProps extends TouchableOpacityProps {
-    title: string
+  title: string;
 }
 
-export function Button({ title,  ...rest } : ButtonProps){
-    return (
-        <TouchableOpacity 
-        style={styles.button}  
-        activeOpacity={0.7}     
-        {...rest}
-        >
-            <Text style={styles.buttonText}>
-                { title }
-            </Text>
-        </TouchableOpacity>
-    )
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} activeOpacity={0.7} {...rest}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
-
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#A370F7',
-        padding: 15,
-        borderRadius: 7,        
-        alignItems: 'center',
-        marginTop: 20
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 17,
-        fontWeight: 'bold',
-    },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
 });
