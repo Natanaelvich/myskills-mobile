6564ed54ddd7ff193bcaa68b532b96eaa7b8208d
Natanael Lima <taelima1997@gmail.com>
2021-07-03 12:36:20 -0300

    add tests E2E home screen

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -3,21 +3,33 @@ describe('Example', () => {
     await device.launchApp();
   });
 
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+  // beforeEach(async () => {
+  //   await device.reloadReactNative();
+  // });
 
   it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+    await expect(element(by.text('Welcome, Natanael'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  it('Shoul add new skill', async () => {
+    await element(by.id('skill-textinput')).clearText()
+    await element(by.id('skill-textinput')).typeText('opaaaaaa');
+    await element(by.id('skill-textinput')).tapReturnKey();
+
+    await element(by.id('button-add')).tap();
+    await element(by.id('skill-textinput')).clearText()
+
+    await expect(element(by.text('opaaaaaa'))).toBeVisible();
+    // await expect(element(by.id('email'))).toNotExist();
   });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('Shoul scroll all list', async () => {
+    await element(by.id('skill-textinput')).typeText('opaaaaaa');
+    await element(by.id('skill-textinput')).tapReturnKey();
+
+    await element(by.id('button-add')).multiTap(12)
+    await element(by.id('skill-textinput')).clearText()
+
+    await element(by.id('FlatList')).scrollTo('bottom');
   });
 });
